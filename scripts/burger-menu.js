738a467761941211803d75a25fe32f82b78e0751
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2024-10-13 20:18:09 +0300

    add projects all files

---
+++ scripts/burger-menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var body = document.body;
+    var button = document.querySelector("svg");
+    var line = document.querySelectorAll("line");
+
+    button.addEventListener("click", function () {
+        if (body.classList.contains("menu-open")) {
+            body.classList.remove("menu-open");
+        } else {
+            body.classList.add("menu-open");
+        }
+    });
+});
